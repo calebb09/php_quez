a48f6471e83dfde553467f75e487e6f6b07997c3
caleb.b00 <calebb090@outlook.com>
2022-06-17 18:58:04 +0300

    first commit

---
+++ php/public/index.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * https://www.php.net/manual/en/curl.examples-basic.php
+ */
+$ini = parse_ini_file('app.ini'); //calling the app.ini file
+$ch = curl_init();   
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);   
+curl_setopt($ch, CURLOPT_URL, $ini['url']); //get data from app.ini
+echo $res = curl_exec($ch);
---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('search-input').addEventListener('keyup', async (e) => {
+    const res = await fetch('./php/public/index.php');
+    console.log(res)
+    const json = await res.json()
+    const key_stroke = document.getElementById("search-input").value
+    let display = '';
+    json.forEach(obj => {
+        if((obj.name.match(key_stroke))){
+            display += '<li>Name: <a href="#">'+obj.name+'</a><BR><BR><span>Email: '+obj.email + '<BR>Body: '+obj.body + '</span></li><hr>'
+        }
+    })
+    const result = display
+    document.getElementById('results').innerHTML = result
+});
