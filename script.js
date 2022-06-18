e65eebad9c051701d48ac5f3e95c414f063e47ad
caleb.b00 <calebb090@outlook.com>
2022-06-18 10:40:48 +0300

    url changed

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.getElementById('search-input').addEventListener('keyup', async (e) => {
-    const res = await fetch('./php/public/index.php');
+    const res = await fetch('./php/server.php');
     console.log(res)
     const json = await res.json()
     const key_stroke = document.getElementById("search-input").value
     let display = '';
     json.forEach(obj => {
         if((obj.name.match(key_stroke))){
-            display += '<li>Name: <a href="#">'+obj.name+'</a><BR><BR><span>Email: '+obj.email + '<BR>Body: '+obj.body + '</span></li><hr>'
+            display += '<li><a href="#">'+obj.name+'</a><BR><BR><span>Email: '+obj.email + '<BR>Body: '+obj.body + '</span></li><hr>'
         }
     })
     const result = display
